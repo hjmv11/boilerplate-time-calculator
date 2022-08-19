40fddac2730ab380cbd5060826b17c7a144fdbb5
hjmv11 <110635467+hjmv11@users.noreply.github.com>
2022-08-19 22:10:55 +0000

    start on time_calc

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,11 @@
-def add_time(start, duration):
+def add_time(start, duration, day=""):
+  start_hour = start.split(':')[0]
+  start_minutes = (start.split(':')[1]).split(' ')[0].lstrip('0')
+  start_ampm = (start.split(':')[1]).split(' ')[1]
+
+  print(start_hour, start_minutes, start_ampm)
 
 
 
+  return new_time
 
-
-    return new_time
